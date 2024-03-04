68cdefff3ac9aced7dececad4d582c21db28929c
Diki Wahyudi <whydiki2308@gmail.com>
2024-03-04 12:53:49 +0700

    Implementasi Search Screen pada aplikasi Pilem

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,24 +1,30 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-class ApiService{
-  static const String baseUrl = 'hhtps://api.themoviedb.org/3';
+class ApiService {
+  static const String baseUrl = "https://api.themoviedb.org/3";
   static const String apiKey = '9b863548472ab5fb588c7a23ba305b24';
-
   Future<List<Map<String, dynamic>>> getAllMovies() async {
-    final response = await http.get(Uri.parse('$baseUrl/movie/now_playing?api.key=$apiKey'));
-    final data = jsonDecode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    final response = await
+    http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
-
   Future<List<Map<String, dynamic>>> getTrendingMovies() async {
-    final response = await http.get(Uri.parse('$baseUrl/movie/week?api.key=$apiKey'));
-    final data = jsonDecode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    final response = await
+    http.get(Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
-
   Future<List<Map<String, dynamic>>> getPopularMovies() async {
-    final response = await http.get(Uri.parse('$baseUrl/movie/popular?api.key=$apiKey'));
-    final data = jsonDecode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    final response = await
+    http.get(Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+  Future<List<Map<String, dynamic>>> searchMovies(String query) async {
+    final response = await
+    http.get(Uri.parse("$baseUrl/search/movie?query=$query&api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
 }
